dbebe867d370064aae9b118f461d44eb57f4a26a
bkitaec <kitaec@list.ru>
2019-04-04 09:01:11 +0200

    add remote mysql credentials

--- api/src/config/env.config.ts
+++ api/src/config/env.config.ts
@@ -2,13 +2,13 @@ import * as dotEnv from 'dotenv';
 dotEnv.config();
 
 export const ENV = {
-    PORT: process.env.PORT || '3000',
+    PORT: process.env.PORT || '1313',
 
-    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_HOST: process.env.DB_HOST || 'remotemysql.com',
     DB_PORT: process.env.DB_PORT || '3306',
-    DB_NAME: process.env.DB_NAME || 'anycafe',
-    DB_USER: process.env.DB_USER || 'anycafe',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'anycafe',
+    DB_NAME: process.env.DB_NAME || 'hWXPCnEMfB',
+    DB_USER: process.env.DB_USER || 'hWXPCnEMfB',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'xmJ0oRYK5x',
     DB_DIALECT: process.env.DB_DIALECT || 'mysql',
 
     JWT_ENCRYPTION: process.env.JWT_ENCRYPTION || 'secureKey',
